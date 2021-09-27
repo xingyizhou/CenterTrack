fac31a6b006decc65a2353d9142166f0e2a7ddf5
YiuKuen <69826761+yktangac@users.noreply.github.com>
2021-09-27 12:58:57 +0800

    Update get_mot_17.sh

--- src/tools/get_mot_17.sh
+++ src/tools/get_mot_17.sh
@@ -1,9 +1,11 @@
-mkdir ../../data/mot17
-cd ../../data/mot17
+mkdir ../../data
+cd ../../data
 wget https://motchallenge.net/data/MOT17.zip
 unzip MOT17.zip
+mv MOT17 mot17
 rm MOT17.zip
+cd mot17
 mkdir annotations
 cd ../../src/tools/
 python convert_mot_to_coco.py
-python convert_mot_det_to_results
+python convert_mot_det_to_results.py
